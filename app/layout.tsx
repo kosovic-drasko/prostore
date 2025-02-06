330e8d9a325fd1de8ec5a1889dd7cc761132e17f
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2025-02-06 21:08:09 +0100

    header footer

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/assets/styles/globals.css';
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from '@/lib/constants/index';
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: `${APP_NAME}`,
+  description: 'A modern store built with Next.js',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
