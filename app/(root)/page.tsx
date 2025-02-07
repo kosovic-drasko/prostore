40f87349240d4ba90cd6696a7538c769b21686b2
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2025-02-07 22:28:10 +0100

    load db neon

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,15 @@
-import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import ProductList from '@/components/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
-const HomePage = () => {
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
   return (
-   <>
-
-      <ProductList title='Najnovije' data={sampleData.products} />
-    </>
+    <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
+      <ProductList title='Newest Arrivals' data={latestProducts} />
+    </div>
   );
 };
+
 export default HomePage;
