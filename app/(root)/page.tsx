f626dd3bb6053ac1fe0e28ff9a2a4d8ae28c221a
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2025-02-08 01:56:24 +0100

    nova kolekcija

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,8 +6,8 @@ const HomePage = async () => {
 
   return (
     <div className='space-y-8'>
-      <h2 className='h2-bold'>Latest Products</h2>
-      <ProductList title='Newest Arrivals' data={latestProducts} />
+      {/* <h2 className='h2-bold'>Latest Products</h2> */}
+      <ProductList title='Nova kolekcija' data={latestProducts} />
     </div>
   );
 };
